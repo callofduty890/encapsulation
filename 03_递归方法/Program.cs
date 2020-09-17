f6212a1f978d3d685cc8cedf101511e14b92c20b
俊华 蔡 <1032031912@qq.com>
2020-09-17 11:23:05 +0800

    1.递归方法 2.迭代器方法

---
+++ 03_递归方法/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_递归方法
+{
+    class RecursionTest
+    {
+        public static  int factorial(int n)
+        {
+            if (n<=1) //递归调用
+            {
+                return 1;
+            }
+            else
+            {
+                return factorial(n - 1) * n;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //递归计算阶乘
+            for (int i = 5; i <= 10; i++)
+            {
+                Console.Write("{0}!={1}\n", i, RecursionTest.factorial(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 04_迭代器方法/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace _04_迭代器方法
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in Fibs())
+            {
+                if (item < 1000)
+                {
+                    Console.Write("{0}\t", item);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> Fibs()
+        {
+            int f1 = 1, f2 = 1;
+            while (true)
+            {
+                yield return f1;
+                yield return f2;
+                f1 += f2;f2 += f1;
+            }
+        }
+
+    }
+}
