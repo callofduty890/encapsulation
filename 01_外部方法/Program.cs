2a05c2b36c9cb5975516e8c4ec757f1d6aa1b525
俊华 蔡 <1032031912@qq.com>
2020-09-17 10:05:06 +0800

    1.函数重载 2.外部方法

---
+++ 01_外部方法/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_外部方法
+{
+    class MyPath
+    {
+        //1.引用空间：using System.Runtime.InteropServices;
+        //2.指定动态链接库：指定DLL的位置（在系统路径中/在当前EXE下/指定路径）
+        [DllImport("kernel32.dll", SetLastError = true)] //引用 C++动态链接库
+        public static extern int GetCurrentDirectory(int BufSize, StringBuilder buf);
+
+
+        [DllImport("kernel32.dll", SetLastError = true)] //引用 C++动态链接库
+        public static extern bool RemoveDirectory(string name);
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder(255);
+
+            MyPath.GetCurrentDirectory(255, sb);
+
+            Console.WriteLine(sb.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
--- 9_17_封装对象/Program.cs
+++ 9_17_封装对象/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace _9_17_封装对象
 {
+    class OverLoadExample
+    {
+        public void SamleMothod(double i)
+        {
+            Console.WriteLine("SamleMothod(double i):{0}", i);
+        }
+        public void SamleMothod(int i)
+        {
+            Console.WriteLine("SamleMothod(int i):{0}", i);
+        }
+        public void SamleMothod(ref int i)
+        {
+            Console.WriteLine("SamleMothod(ref int i):{0}", i);
+        }
+        //public void SamleMothod(out int i) { }; //编译错误
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            OverLoadExample Over = new OverLoadExample();
+            int i = 10;double d = 11.1;
+            Over.SamleMothod(i); //调用SamleMothod(int i)
+            Over.SamleMothod(d);//调用SamleMothod(double i)
+            Over.SamleMothod(ref i);//SamleMothod(ref int i)
+            Console.ReadKey();
+
         }
     }
 }
